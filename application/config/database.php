7eae7a786f4ef47a76d270fc33ed32d0da3e56e0
Kevin Brammer <kbrammer@gmail.com>
2014-01-11 19:24:48 -0600

    updated database.config for PDO

--- application/config/database.php
+++ application/config/database.php
@@ -2,30 +2,29 @@
 
 	return array
 	(
-		'default' => array
-		(
+		'default' => array(
 			'type'       => 'PDO',
 			'connection' => array(
 				/**
-				 * The following options are available for MySQL:
+				 * The following options are available for PDO:
 				 *
-				 * string   hostname     server hostname, or socket
-				 * string   database     database name
-				 * string   username     database username
-				 * string   password     database password
-				 * boolean  persistent   use persistent connections?
-				 * array    variables    system variables as "key => value" pairs
-				 *
-				 * Ports and sockets may be appended to the hostname.
+				 * string   dsn         Data Source Name
+				 * string   username    database username
+				 * string   password    database password
+				 * boolean  persistent  use persistent connections?
 				 */
-				'hostname'   => 'us-cdbr-azure-west-b.cleardb.com',
-				'database'   => 'kevinbrAtnANkevU',
+				'dsn'        => 'mysql:host=us-cdbr-azure-west-b.cleardb.com;dbname=kevinbrAtnANkevU',
 				'username'   => 'b10fddaa4b278c',
 				'password'   => '8cfbe4a9',
 				'persistent' => FALSE,
 			),
+			/**
+			 * The following extra options are available for PDO:
+			 *
+			 * string   identifier  set the escaping identifier
+			 */
 			'table_prefix' => '',
 			'charset'      => 'utf8',
 			'caching'      => FALSE,
-		)
+		),
 	);
