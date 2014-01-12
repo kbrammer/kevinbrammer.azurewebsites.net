93934e31de3f4259071cbeb571de419024e4dc0a
Kevin Brammer <kbrammer@gmail.com>
2014-01-12 13:10:43 -0600

    added ace editor to admin

--- application/views/admin/edit.php
+++ application/views/admin/edit.php
@@ -1,3 +1,16 @@
+<style type="text/css" media="screen">
+    /*#editor { 
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+    }*/
+    #editor {
+    	min-height: 350px;
+    	width: 100%;
+    }
+</style>
 <div class="row">
 	<div class="col-md-12">
 		<h2>Edit Post</h2>
@@ -16,7 +29,8 @@
 		<?php echo Form::input('excerpt', $post->excerpt, array('class' => 'form-control')); ?>
 		
 		<?php echo Form::label('content', 'Content'); ?>
-		<?php echo Form::textarea('content', $post->content, array('class' => 'form-control')); ?>
+		<?php echo Form::textarea('content', $post->content, array('class' => 'form-control', 'id' => 'post_content')); ?>
+		<div id="editor"></div>
 
 		<br>
 
@@ -24,7 +38,7 @@
 		<a href="<?= URL::site('blog/detail/' . $post->url_title) ?>" role="button" target="_blank">Preview</a>
 		<?php endif; ?>
 
-		<?php echo Form::submit(NULL, 'Save', array('class' => 'btn btn-primary pull-right')); ?>
+		<?php echo Form::submit(NULL, 'Save', array('class' => 'btn btn-primary pull-right', 'id' => 'save_post')); ?>
 
 		<?php echo Form::close(); ?>
 		<br>
@@ -42,3 +56,30 @@
 	</div>
 </div>
 <?php endif; ?>
+
+<script src="<?= URL::base() ?>assets/js/ace/ace.js" type="text/javascript" charset="utf-8"></script>
+<script>
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/xcode");
+    editor.getSession().setMode("ace/mode/html");
+    editor.getSession().setUseWrapMode(true);
+
+    // get textarea content for ace
+    var post_content = document.getElementById('post_content');
+    
+    // hide textarea so that we only see ace
+    post_content.style.display = 'none';
+    
+    // set ace with content from textarea
+    editor.setValue(post_content.value);
+    
+    // get save button
+    var save_button = document.getElementById('save_post');
+    
+    // when save is clicked, update textarea with value from ace
+    save_button.onclick = function(e){
+    	// e.preventDefault();
+    	post_content.value = editor.getValue();
+    	// return false;
+    };
+</script>
